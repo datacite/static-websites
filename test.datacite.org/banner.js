5ad492e172cf338c8e87f6f9903e2ca5e97a028b
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-04-13 14:25:14 +0200

    add developer bar

---
+++ test.datacite.org/banner.js
@@ -0,0 +1,32 @@
+var datacite_services = [ {
+	"path" : "/mds",
+	"name" : "Metadata Store"
+}, {
+	"path" : "/search",
+	"name" : "Search"
+}, {
+	"path" : "/schema",
+	"name" : "Schema"
+}, {
+	"path" : "/oaip",
+	"name" : "OAI-PMH"
+}, {
+	"path" : "/data",
+	"name" : "Content Resolver"
+}, {
+	"path" : "/handle",
+	"name" : "Handle Server"
+} ];
+
+$(document).ready(function() {
+	var span = $("<span>").text("DataCite Test Services");
+	var ul = $("<ul>");
+	$(datacite_services).each(function(idx, service){
+		var li = $("<li>");
+		var a = $("<a>").text(service.name).attr("href", service.path);
+		li.append(a);
+		ul.append(li);
+	});
+	
+	$("#banner").append(span, ul);
+})
