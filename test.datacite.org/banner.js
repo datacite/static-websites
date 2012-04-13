0059ea29770c496c214d2a3a650e0c4047e543f5
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-04-13 15:15:45 +0200

    indent code

--- test.datacite.org/banner.js
+++ test.datacite.org/banner.js
@@ -1,37 +1,36 @@
 var banner_jquery = $.noConflict(true);
 
 (function($) {
+	var datacite_services = [ {
+		"path" : "/mds",
+		"name" : "Metadata Store"
+	}, {
+		"path" : "/search",
+		"name" : "Search"
+	}, {
+		"path" : "/schema",
+		"name" : "Schema"
+	}, {
+		"path" : "/oaip",
+		"name" : "OAI-PMH"
+	}, {
+		"path" : "/data",
+		"name" : "Content Resolver"
+	}, {
+		"path" : "/handle",
+		"name" : "Handle Server"
+	} ];
 
-var datacite_services = [ {
-	"path" : "/mds",
-	"name" : "Metadata Store"
-}, {
-	"path" : "/search",
-	"name" : "Search"
-}, {
-	"path" : "/schema",
-	"name" : "Schema"
-}, {
-	"path" : "/oaip",
-	"name" : "OAI-PMH"
-}, {
-	"path" : "/data",
-	"name" : "Content Resolver"
-}, {
-	"path" : "/handle",
-	"name" : "Handle Server"
-} ];
+	$(document).ready(function() {
+		var span = $("<span>").text("DataCite Test Services");
+		var ul = $("<ul>");
+		$(datacite_services).each(function(idx, service) {
+			var li = $("<li>");
+			var a = $("<a>").text(service.name).attr("href", service.path);
+			li.append(a);
+			ul.append(li);
+		});
 
-$(document).ready(function() {
-	var span = $("<span>").text("DataCite Test Services");
-	var ul = $("<ul>");
-	$(datacite_services).each(function(idx, service){
-		var li = $("<li>");
-		var a = $("<a>").text(service.name).attr("href", service.path);
-		li.append(a);
-		ul.append(li);
-	});
-	
-	$("#banner").append(span, ul);
-})
+		$("#banner").append(span, ul);
+	})
 })(banner_jquery);
